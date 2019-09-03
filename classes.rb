e51ad899b25ef01ea88e9c5948807d4d311360a7
techspeterson <tpeterson.engsci@gmail.com>
2019-09-03 12:33:42 +1000

    card lists populated

---
+++ classes.rb
@@ -0,0 +1,7 @@
+class Player
+  @@cards_char = ["Miss Scarlet", "Colonel Mustard", "Professor Plum", "Mr. Green", "Mrs. Peacock", "Mrs. White"]
+  @@cards_room = ["Study", "Kitchen", "Ballroom", "Conservatory", "Billiard Room", "Library", "Hall", "Lounge", "Dining Room", "Cellar"]
+  @@cards_weapons = ["Candlestick", "Knife", "Lead Pipe", "Revolver", "Rope", "Wrench"]
+  def initialize
+  end
+end
