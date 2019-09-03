8c621cd24eb1a2e29df56d6c6592b36dae6ffeff
techspeterson <tpeterson.engsci@gmail.com>
2019-09-03 13:48:33 +1000

    refactoring card list/player class

--- classes.rb
+++ classes.rb
@@ -1,7 +1,26 @@
 class Player
-  @@cards_char = ["Miss Scarlet", "Colonel Mustard", "Professor Plum", "Mr. Green", "Mrs. Peacock", "Mrs. White"]
-  @@cards_room = ["Study", "Kitchen", "Ballroom", "Conservatory", "Billiard Room", "Library", "Hall", "Lounge", "Dining Room", "Cellar"]
-  @@cards_weapons = ["Candlestick", "Knife", "Lead Pipe", "Revolver", "Rope", "Wrench"]
-  def initialize
+  @@cards = {
+    suspects: ["Miss Scarlet", "Colonel Mustard", "Professor Plum", "Rev. Green", "Mrs. Peacock", "Mrs. White"],
+    rooms: ["Study", "Kitchen", "Ballroom", "Conservatory", "Billiard Room", "Library", "Hall", "Lounge", "Dining Room"],
+    weapons: ["Candlestick", "Dagger", "Lead Pipe", "Revolver", "Rope", "Spanner"]
+  }
+
+  @@character_list = Array.new(@@cards[:suspects])
+  @@envelope_cards = {
+    suspect: nil,
+    room: nil,
+    weapon: nil
+  }
+
+  @user = false
+
+  def initialize(is_user)
+    if is_user
+      @character = @@character_list[0]
+      @@character_list.delete_at(0)
+    else
+      @character = @@character_list.sample
+      @@character_list.delete(@character)
+    end
   end
 end
